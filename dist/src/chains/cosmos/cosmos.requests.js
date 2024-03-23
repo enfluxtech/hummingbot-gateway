b038e74956e77daf4bbd81069e2d36f517aacd21
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2024-03-23 20:17:11 +0800

    Delete dist directory

--- dist/src/chains/cosmos/cosmos.requests.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TransactionResponseStatusCode = void 0;
-var TransactionResponseStatusCode;
-(function (TransactionResponseStatusCode) {
-    TransactionResponseStatusCode[TransactionResponseStatusCode["FAILED"] = -1] = "FAILED";
-    TransactionResponseStatusCode[TransactionResponseStatusCode["CONFIRMED"] = 1] = "CONFIRMED";
-})(TransactionResponseStatusCode || (exports.TransactionResponseStatusCode = TransactionResponseStatusCode = {}));
-//# sourceMappingURL=cosmos.requests.js.map
--- dist/src/connectors/quickswap/quickswap.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QuickswapConfig = void 0;
-const network_utils_1 = require("../../network/network.utils");
-var QuickswapConfig;
-(function (QuickswapConfig) {
-    QuickswapConfig.config = (0, network_utils_1.buildConfig)('quickswap', ['AMM'], [{ chain: 'polygon', networks: ['mainnet', 'mumbai'] }], 'EVM');
-})(QuickswapConfig || (exports.QuickswapConfig = QuickswapConfig = {}));
-//# sourceMappingURL=quickswap.config.js.map
